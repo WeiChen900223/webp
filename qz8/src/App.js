80a95e6ed0fe23d1df539ccd6fae931770b71872
WeiChen900223 <100269349+WeiChen900223@users.noreply.github.com>
2022-05-17 23:14:32 +0800

    Add files via upload

---
+++ qz8/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import multiButton from './cgu_multiButton'
+
+function App() {
+  return (
+    <div className="App">
+    <div>{multiButton(10)}</div>
+    </div>
+  );
+}
+
+export default App;
